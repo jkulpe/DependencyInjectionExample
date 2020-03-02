273c9080a46efe00906b80ddc7def7becdba4215
Jason Kulpe <jkulpe@gmail.com>
2020-03-02 05:18:31 -0600

    First commit

---
+++ CompositionRoot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInjectionExample
+{
+    public class CompositionRoot : ICompositionRoot
+    {
+        // We will do fancier stuff here in just a bit...
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+
+namespace DependencyInjectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new WindsorContainer();
+
+            // Register the CompositionRoot type with the container
+            container.Register(Component.For<ICompositionRoot>().ImplementedBy<CompositionRoot>());
+
+            // Resolve an object of type ICompositionRoot (ask the container for an instance)
+            // This is analogous to calling new() in a non-IoC application
+
+
+        }
+    }
+}
